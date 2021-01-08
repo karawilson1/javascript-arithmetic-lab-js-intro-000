95e29461e65e34f506ebecc8f3c404dc9eb4c77b
Wilson <karaewilson16@gmail.com>
2021-01-08 14:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function makeInt(n) {
   return (parseInt(n, 10))
 }
 
-function preserveDecimal('n') {
+function preserveDecimal(n) {
   if ('n' === 'decimal')
   return (number.parseFloat())
   else return "NaN"
